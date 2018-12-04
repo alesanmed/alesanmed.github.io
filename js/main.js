643f5253a1634c948481382d0d43b35ceddc5555
Ale Sánchez <alesanchez@us.es>
2018-12-04 15:51:41 +0100

    Added new sections
    Added new sections and divisions along website

--- js/main.js
+++ js/main.js
@@ -2,11 +2,26 @@
     $(function(){
         $('.parallax').parallax();
 
-        $(".bar").each(function(){
-            $(this).find(".bar-inner").animate({
-                width: $(this).attr("data-width")
-            },2000)
-        });
+        var $win = $(window);
+        var $stat = $('.section-technologies');
+
+        $win.on('scroll', function () {
+            var scrollTop = $win.scrollTop();
+        
+            $stat.each(function () {
+                var $self = $(this);
+                var prev=$self.offset();
+                console.log("Scrolltop: " + scrollTop)
+                console.log("Prevtop: " + prev.top)
+                if ( (scrollTop - prev.top) > -1000) {
+                    $('.bar').each(function() {
+                        $(this).find(".bar-inner").animate({
+                            width: $(this).attr("data-width")
+                        },2000)
+                    })
+                }
+            });
+        }).scroll();
     }); // end of document ready
 })(jQuery); // end of jQuery name space
   
